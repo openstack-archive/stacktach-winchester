02206d58fbf37fec90633966d844336308792906
Sandy Walsh <sandy.walsh@rackspace.com>
2014-10-07 11:23:05 -0700

    Support for playback of old events.
    Introduces the time_sync object which can sync
    time with an external time service (like the time_sync
    service in notigen).
    
    This is used for playback of old events. It prevents
    premature expiry triggers.
    
    Also some tweaks to the UsageHandler, work in progress
    and used for testing.
    
    Change-Id: I45033fbd9c12d98f96816a4f90cf7dc8c915ef51

--- winchester/pipeline_handler.py
+++ winchester/pipeline_handler.py
@@ -90,3 +90,175 @@ class LoggingHandler(PipelineHandlerBase):
         pass
 
 
+class UsageException(Exception):
+    pass
+
+
+class UsageHandler(PipelineHandlerBase):
+    def _find_exists(self, events):
+        exists = None
+
+        # We could have several .exists records, but only the
+        # end-of-day .exists will have audit_period_* time of
+        # 00:00:00 and be 24hrs apart.
+        for event in events:
+            apb = event.get('audit_period_beginning')
+            ape = event.get('audit_period_ending')
+            if (event['event_type'] == 'compute.instance.exists'
+                and apb and ape and apb.time() == datetime.time(0, 0, 0)
+                and ape.time() == datetime.time(0, 0, 0)
+                and ape.date() == (apb.date() + datetime.timedelta(days=1))):
+                exists = event
+                self.audit_beginning = apb
+                self.audit_ending = ape
+                break
+
+        if not exists:
+            raise UsageException("No .exists record.")
+
+        return exists
+
+    def _extract_launched_at(self, exists):
+        if not exists['launched_at']:
+            raise UsageException(".exists has no launched_at value.")
+        return exists['launched_at']
+
+    def _extract_interesting_events(self, events, interesting):
+        return [event for event in events
+                        if event['event_type'] in interesting]
+
+    def _find_events(self, events):
+        interesting = ['compute.instance.rebuild.start',
+                       'compute.instance.resize.prep.start',
+                       'compute.instance.resize.revert.start',
+                       'compute.instance.rescue.start',
+                       'compute.instance.create.end',
+                       'compute.instance.rebuild.end',
+                       'compute.instance.resize.finish.end',
+                       'compute.instance.resize.revert.end',
+                       'compute.instance.rescue.end']
+
+        # We could easily end up with no events in final_set if
+        # there were no operations performed on an instance that day.
+        # We'll still get a .exists for every active instance though.
+
+        return self._extract_interesting_events(events, interesting)
+
+    def _find_deleted_events(self, events):
+        interesting = ['compute.instance.delete.end']
+        return self._extract_interesting_events(events, interesting)
+
+    def _verify_fields(self, exists, launch, fields):
+        for field in fields:
+            if field not in exists and field not in launch:
+                continue
+            if exists[field] != launch[field]:
+                raise UsageException("Conflicting '%s' values ('%s' != '%s')"
+                                % (field, exists[field], launch[field]))
+
+    def _confirm_delete(self, exists, deleted, fields):
+        deleted_at = exists.get('deleted_at')
+        launched_at = exists.get('launched_at')
+        state = exists.get('state')
+
+        if deleted_at and state != "deleted":
+            raise UsageException(".exists state not 'deleted' but "
+                             "deleted_at is set.")
+
+        if deleted_at and not deleted:
+            # We've already confirmed it's in the "deleted" state.
+            if deleted_at < launched_at:
+                raise UsageException(".exists deleted_at < launched_at.")
+
+            # Is the deleted_at within this audit period?
+            if (deleted_at >= self.audit_beginning
+                                and deleted_at <= self.audit_ending):
+                raise UsageException(".exists deleted_at in audit "
+                    "period, but no matching .delete event found.")
+
+        if not deleted_at and deleted:
+            raise UsageException(".deleted events found but .exists has "
+                                 "no deleted_at value.")
+
+        if len(deleted) > 1:
+            raise UsageException("Multiple .delete.end events")
+
+        if deleted:
+            self._verify_fields(exists, deleted[0],  fields)
+
+    def _confirm_launched_at(self, exists, events):
+        if exists.get('state') != 'active':
+            return
+
+        # Does launched_at have a value within this audit period?
+        # If so, we should have a related event. Otherwise, this
+        # instance was created previously.
+        launched_at = exists['launched_at']
+        if (launched_at >= self.audit_beginning
+                and launched_at <= self.audit_ending and len(events) == 1):
+            raise UsageException(".exists launched_at in audit "
+                "period, but no related events found.")
+
+        # TODO(sandy): Confirm the events we got set launched_at
+        # properly.
+
+    def _get_core_fields(self):
+        """Broken out so derived classes can define their
+           own trait list."""
+        return ['launched_at', 'instance_type_id', 'tenant_id',
+                'os_architecture', 'os_version', 'os_distro']
+
+    def handle_events(self, events, env):
+        self.env = env
+        self.stream_id = env['stream_id']
+
+        delete_fields = ['launched_at', 'deleted_at']
+        core_fields = self._get_core_fields()
+
+        exists = None
+        error = None
+        try:
+            exists = self._find_exists(events)
+            launched_at = self._extract_launched_at(exists)
+            deleted = self._find_deleted_events(events)
+            close = self._find_events(events)
+            for c in close:
+                self._verify_fields(exists, c, core_fields)
+
+            self._confirm_launched_at(exists, events)
+            self._confirm_delete(exists, deleted, delete_fields)
+            event_type = "compute.instance.exists.verified"
+        except UsageException as e:
+            error = str(e)
+            logger.warn("Stream %s UsageException: %s" % (self.stream_id, e))
+            logger.warn("deleted_at: %s, launched_at: %s, state: %s, APB: "
+                        "%s, APE: %s" % (exists.get("deleted_at"),
+                        exists.get("launched_at"), exists.get("state"),
+                        exists.get("audit_period_beginning"),
+                        exists.get("audit_period_ending")))
+            event_type = "compute.instance.exists.failed"
+
+        if len(events) > 1:
+            logger.warn("Events for Steam: %s" % self.stream_id)
+            for event in events:
+                logger.warn("^Event: %s - %s" % (event['timestamp'], event['event_type']))
+
+        if exists:
+            new_event = {'event_type': event_type,
+                         'message_id': str(uuid.uuid4()),
+                         'timestamp': exists.get('timestamp',
+                                                datetime.datetime.utcnow()),
+                         'stream_id': int(self.stream_id),
+                         'instance_id': exists.get('instance_id'),
+                         'error': error
+                        }
+            #events.append(new_event)
+        else:
+            logger.debug("No .exists record")
+        return events
+
+    def commit(self):
+        pass
+
+    def rollback(self):
+        pass
--- winchester/yagi_handler.py
+++ winchester/yagi_handler.py
@@ -5,6 +5,7 @@ import yagi.config
 
 from winchester.trigger_manager import TriggerManager
 from winchester.config import ConfigManager
+from winchester import time_sync
 
 
 logger = logging.getLogger(__name__)
@@ -22,10 +23,13 @@ class WinchesterHandler(BaseHandler):
         super(WinchesterHandler, self).__init__(app=app, queue_name=queue_name)
         conf_file = self.config_get("config_file")
         config = ConfigManager.load_config_file(conf_file)
-        self.trigger_manager = TriggerManager(config)
+        self.time_sync = time_sync.TimeSync(config, publishes=True)
+        self.trigger_manager = TriggerManager(config, time_sync=self.time_sync)
 
     def handle_messages(self, messages, env):
         for notification in self.iterate_payloads(messages, env):
+            tyme = notification['timestamp']
+            self.time_sync.publish(tyme)
             self.trigger_manager.add_notification(notification)
 
     def on_idle(self, num_messages, queue_name):
