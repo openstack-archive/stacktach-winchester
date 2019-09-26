ca4ece0cc50e80ac2a46b0ae17dba6d1b6b4b188
Jim Rollenhagen <jim@jimrollenhagen.com>
2019-09-26 09:58:05 -0400

    Retire github mirror, repo moved to opendev

--- winchester/db/__init__.py
+++
@@ -1,3 +0,0 @@
-from winchester.db.interface import DuplicateError, LockError, DatabaseConnectionError
-from winchester.db.interface import NoSuchEventError, NoSuchStreamError
-from winchester.db.interface import DBInterface
